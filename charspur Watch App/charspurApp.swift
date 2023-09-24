9fd1e495815da873e058206606307e274d71fe87
laurencary <armstronglg720@gmail.com>
2023-09-24 16:55:50 -0700

    Initial Commit

---
+++ charspur Watch App/charspurApp.swift
@@ -0,0 +1,17 @@
+//
+//  charspurApp.swift
+//  charspur Watch App
+//
+//  Created by Lauren Armstrong on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct charspur_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
